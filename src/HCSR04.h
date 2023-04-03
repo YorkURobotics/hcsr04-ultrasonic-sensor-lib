d743b6a194b79d4522efc30bed650b3d1136d696
BarbodH <habibi.k.barbod@gmail.com>
2023-04-03 15:01:26 -0400

    Updated header file documentation

--- src/HCSR04.h
+++ src/HCSR04.h
@@ -4,15 +4,17 @@
 class HCSR04
 {
 public:
-	HCSR04(int out, int echo);			//initialisation class HCSR04 (trig pin , echo pin)
-	HCSR04(int out, int echo[], int n); //initialisation class HCSR04 (trig pin , echo pin)
-	~HCSR04();							//destructor
-	float dist() const;					//return curent distance of element 0
-	float dist(int n) const;			//return curent distance of element n
+	// overloaded constructors & destructor
+	HCSR04(int out, int echo);
+	HCSR04(int out, int echo[], int n);
+	~HCSR04();
+	// utility functions
+	float dist() const;
+	float dist(int n) const;
 
 private:
-	void init(int out, int echo[], int n); //for constructor
-	int out;							   //out pin
-	int *echo;							   //echo pin list
-	int n;								   //number of el
+	void init(int out, int echo[], int n);
+	int out;	// ouput pin
+	int *echo;	// echo pin(s)
+	int n;	// number of sensors, i.e., echo pins
 };
